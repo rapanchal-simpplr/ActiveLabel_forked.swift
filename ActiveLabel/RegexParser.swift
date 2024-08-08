0329452f0d6a88b933c6125d4b091bd569a03ef3
rapanchal-simpplr <102642022+rapanchal-simpplr@users.noreply.github.com>
2024-08-08 17:44:03 +0530

    Update RegexParser.swift
    Added error handling in case where regular expression fails to create.

--- ActiveLabel/RegexParser.swift
+++ ActiveLabel/RegexParser.swift
@@ -19,19 +19,18 @@ struct RegexParser {
 
     private static var cachedRegularExpressions: [String : NSRegularExpression] = [:]
 
-    static func getElements(from text: String, with pattern: String, range: NSRange) -> [NSTextCheckingResult]{
-        guard let elementRegex = regularExpression(for: pattern) else { return [] }
+    static func getElements(from text: String, with pattern: String, range: NSRange) throws -> [NSTextCheckingResult] {
+        guard let elementRegex = try regularExpression(for: pattern) else { return [] }
         return elementRegex.matches(in: text, options: [], range: range)
     }
 
-    private static func regularExpression(for pattern: String) -> NSRegularExpression? {
+    private static func regularExpression(for pattern: String) throws -> NSRegularExpression? {
         if let regex = cachedRegularExpressions[pattern] {
             return regex
-        } else if let createdRegex = try? NSRegularExpression(pattern: pattern, options: [.caseInsensitive]) {
+        } else {
+            let createdRegex = try NSRegularExpression(pattern: pattern, options: [.caseInsensitive])
             cachedRegularExpressions[pattern] = createdRegex
             return createdRegex
-        } else {
-            return nil
         }
     }
 }
