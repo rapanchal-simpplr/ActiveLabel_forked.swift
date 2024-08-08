b9bea1ced0b0f912f03f77ca579f112ef5ea0079
rapanchal-simpplr <102642022+rapanchal-simpplr@users.noreply.github.com>
2024-08-08 17:42:13 +0530

    Update ActiveBuilder.swift
    Added error handling when regular expression fails for matching pattern.

--- ActiveLabel/ActiveBuilder.swift
+++ ActiveLabel/ActiveBuilder.swift
@@ -26,77 +26,104 @@ struct ActiveBuilder {
     }
 
     static func createURLElements(from text: String, range: NSRange, maximumLength: Int?) -> ([ElementTuple], String) {
-        let type = ActiveType.url
-        var text = text
-        let matches = RegexParser.getElements(from: text, with: type.pattern, range: range)
-        let nsstring = text as NSString
-        var elements: [ElementTuple] = []
+        do {
+            let type = ActiveType.url
+            var text = text
+            let nsstring = text as NSString
+            var elements: [ElementTuple] = []
 
-        for match in matches where match.range.length > 2 {
-            let word = nsstring.substring(with: match.range)
-                .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+            let matches = try RegexParser.getElements(from: text, with: type.pattern, range: range)
 
-            guard let maxLength = maximumLength, word.count > maxLength else {
-                let range = maximumLength == nil ? match.range : (text as NSString).range(of: word)
-                let element = ActiveElement.create(with: type, text: word)
-                elements.append((range, element, type))
-                continue
+            for match in matches where match.range.length > 2 {
+                let word = nsstring.substring(with: match.range)
+                    .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+
+                guard let maxLength = maximumLength, word.count > maxLength else {
+                    let range = maximumLength == nil ? match.range : (text as NSString).range(of: word)
+                    let element = ActiveElement.create(with: type, text: word)
+                    elements.append((range, element, type))
+                    continue
+                }
+
+                let trimmedWord = word.trim(to: maxLength)
+                text = text.replacingOccurrences(of: word, with: trimmedWord)
+
+                let newRange = (text as NSString).range(of: trimmedWord)
+                let element = ActiveElement.url(original: word, trimmed: trimmedWord)
+                elements.append((newRange, element, type))
             }
 
-            let trimmedWord = word.trim(to: maxLength)
-            text = text.replacingOccurrences(of: word, with: trimmedWord)
-
-            let newRange = (text as NSString).range(of: trimmedWord)
-            let element = ActiveElement.url(original: word, trimmed: trimmedWord)
-            elements.append((newRange, element, type))
+            return (elements, text)
+        } catch let _ {
+            return ([], "")
         }
-        return (elements, text)
     }
 
     private static func createElements(from text: String,
-                                            for type: ActiveType,
-                                                range: NSRange,
-                                                minLength: Int = 2,
-                                                filterPredicate: ActiveFilterPredicate?) -> [ElementTuple] {
+                                       for type: ActiveType,
+                                       range: NSRange,
+                                       minLength: Int = 2,
+                                       filterPredicate: ActiveFilterPredicate?) -> [ElementTuple] {
 
-        let matches = RegexParser.getElements(from: text, with: type.pattern, range: range)
         let nsstring = text as NSString
-        var elements: [ElementTuple] = []
 
-        for match in matches where match.range.length > minLength {
-            let word = nsstring.substring(with: match.range)
-                .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-            if filterPredicate?(word) ?? true {
-                let element = ActiveElement.create(with: type, text: word)
-                elements.append((match.range, element, type))
+        do {
+            var elements: [ElementTuple] = []
+            let matches = try RegexParser.getElements(from: text, with: type.pattern, range: range)
+
+            for match in matches where match.range.length > minLength {
+                let word = nsstring.substring(with: match.range)
+                    .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+                if filterPredicate?(word) ?? true {
+                    let element = ActiveElement.create(with: type, text: word)
+                    elements.append((match.range, element, type))
+                }
+            }
+
+            return elements
+
+        } catch let _ {
+            let plainPatternString = type.pattern.replacingOccurrences(of: "\\s", with: "").replacingOccurrences(of: "\\b", with: "")
+            let range = nsstring.range(of: plainPatternString, options: [.caseInsensitive])
+
+            if filterPredicate?(plainPatternString) ?? true {
+                let element = ActiveElement.create(with: type, text: plainPatternString)
+                return [(range, element, type)]
+            } else {
+                return []
             }
         }
-        return elements
     }
 
     private static func createElementsIgnoringFirstCharacter(from text: String,
-                                                                  for type: ActiveType,
-                                                                      range: NSRange,
-                                                                      filterPredicate: ActiveFilterPredicate?) -> [ElementTuple] {
-        let matches = RegexParser.getElements(from: text, with: type.pattern, range: range)
-        let nsstring = text as NSString
-        var elements: [ElementTuple] = []
+                                                             for type: ActiveType,
+                                                             range: NSRange,
+                                                             filterPredicate: ActiveFilterPredicate?) -> [ElementTuple] {
+        do {
+            var elements: [ElementTuple] = []
+            let nsstring = text as NSString
 
-        for match in matches where match.range.length > 2 {
-            let range = NSRange(location: match.range.location + 1, length: match.range.length - 1)
-            var word = nsstring.substring(with: range)
-            if word.hasPrefix("@") {
-                word.remove(at: word.startIndex)
-            }
-            else if word.hasPrefix("#") {
-                word.remove(at: word.startIndex)
+            let matches = try RegexParser.getElements(from: text, with: type.pattern, range: range)
+
+            for match in matches where match.range.length > 2 {
+                let range = NSRange(location: match.range.location + 1, length: match.range.length - 1)
+                var word = nsstring.substring(with: range)
+                if word.hasPrefix("@") {
+                    word.remove(at: word.startIndex)
+                }
+                else if word.hasPrefix("#") {
+                    word.remove(at: word.startIndex)
+                }
+
+                if filterPredicate?(word) ?? true {
+                    let element = ActiveElement.create(with: type, text: word)
+                    elements.append((match.range, element, type))
+                }
             }
 
-            if filterPredicate?(word) ?? true {
-                let element = ActiveElement.create(with: type, text: word)
-                elements.append((match.range, element, type))
-            }
+            return elements
+        } catch {
+            return []
         }
-        return elements
     }
 }
